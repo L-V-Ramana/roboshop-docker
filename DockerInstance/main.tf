e3ff47d840e8097777ac7b52fa5be62cf6e1fe2e
L V Ramana <lellalvramana@gmail.com>
2025-12-19 18:05:28 +0530

    docker

---
+++ DockerInstance/main.tf
@@ -0,0 +1,32 @@
+resource"aws_instance""DockerHost"{
+    ami = data.aws_ami.joindevops.id
+    instance_type = "t3.medium"
+    vpc_security_group_ids = [aws_security_group.docker.id]
+    tags={
+        Name = "Docker_instance_roboshop"
+    }
+
+    user_data =file("docker.sh")
+}
+
+resource "aws_security_group""docker"{
+    name = "Docker"
+    description = "for docker"
+
+    ingress{
+        from_port=0
+        to_port=0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+
+    }
+
+    egress{
+        from_port=0
+        to_port=0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]  
+    }
+}
